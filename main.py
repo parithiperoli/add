6865557784cbc69889ecbba734743fba30b7769d
parithiperoli <142210460+parithiperoli@users.noreply.github.com>
2023-08-14 13:51:27 +0530

    camera

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def add(a, b):
 st.write(" # This is my simple webapp")
 num1 = st.number_input('Pick a number 1')
 num2 = st.number_input('Pick a number 2')
+st.camera_input("Take a picture")
 data = st.selectbox('Pick one', ['cats', 'dogs'])
 
 if st.button("Add"):
