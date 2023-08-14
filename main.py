d4f094ba61b81836880ed1c2330b82f586d125c6
parithiperoli <142210460+parithiperoli@users.noreply.github.com>
2023-08-14 13:44:27 +0530

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def add(a, b):
+    return a+b
+
+num1 = st.number_input('Pick a number 1')
+num2 = st.number_input('Pick a number 2')
+data = st.selectbox('Pick one', ['cats', 'dogs'])
+
+if st.button("Add"):
+    st.write(add(num1,num2))
+    st.write(data)
